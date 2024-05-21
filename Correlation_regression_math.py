917b509b62732f2d6c085ce57c9835c327a7188a
mirolongo <70956059+mirolongo@users.noreply.github.com>
2024-05-21 11:07:13 +0200

    Update Correlation_regression_math.py

--- Correlation_regression_math.py
+++ Correlation_regression_math.py
@@ -3,10 +3,8 @@ import numpy as np
 from scipy.stats import pearsonr
 import statsmodels.api as sm
 
-# Carregar os dados
 df = pd.read_csv(r'C:/Users/miro-/OneDrive/Documentos/University - Coventry-FEI/Subjects/Thesis/Findings/graphics/coded_data.csv')
 
-# Função para cálculo de correlação e regressão
 def calculate_correlation_and_regression(x, y):
     correlation, _ = pearsonr(x, y)
     x = sm.add_constant(x)
@@ -16,21 +14,18 @@ def calculate_correlation_and_regression(x, y):
     regression_p_value = model.pvalues.iloc[1]
     return correlation, regression_slope, regression_intercept, regression_p_value
 
-# Cálculo para Frequency of Use vs. Advancement in Studies Perception
 x1 = df['Frequency of use']
 y1 = df['Advancement in Studies Perception']
 correlation1, slope1, intercept1, p_value1 = calculate_correlation_and_regression(x1, y1)
 print(f'Correlation between Frequency of Use and Advancement in Studies Perception: r = {correlation1}')
 print(f'Regression slope: {slope1}, intercept: {intercept1}, p-value: {p_value1}')
 
-# Cálculo para Perception of Improvement vs. Influence to Complete Academic Tasks
 x2 = df['Perception of Improvement']
 y2 = df['Influence to complete Academic task']
 correlation2, slope2, intercept2, p_value2 = calculate_correlation_and_regression(x2, y2)
 print(f'Correlation between Perception of Improvement and Influence to Complete Academic Tasks: r = {correlation2}')
 print(f'Regression slope: {slope2}, intercept: {intercept2}, p-value: {p_value2}')
 
-# Verificar se a coluna 'Reported Benefits' existe no DataFrame
 if 'Reported Benefits' in df.columns:
     x3 = df['Frequency of use']
     y3 = df['Reported Benefits']
@@ -38,4 +33,4 @@ if 'Reported Benefits' in df.columns:
     print(f'Correlation between Frequency of Use and Reported Benefits: r = {correlation3}')
     print(f'Regression slope: {slope3}, intercept: {intercept3}, p-value: {p_value3}')
 else:
-    print("A coluna 'Reported Benefits' não existe no DataFrame.")
+    print("The colum 'Reported Benefits' does not exist in DataFrame.")
