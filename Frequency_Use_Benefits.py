e7032237e7bb21a105f4c5e2813d67ee81b5a2bf
mirolongo <70956059+mirolongo@users.noreply.github.com>
2024-05-21 11:29:35 +0200

    Update Frequency_Use_Benefits.py

--- Frequency_Use_Benefits.py
+++ Frequency_Use_Benefits.py
@@ -1,27 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Dados fornecidos
+
 data = {
     "Frequency of use": [4, 4, 5, 2, 3, 5, 2, 4, 3, 5, 3, 2, 5, 3, 4, 3, 4, 4, 3, 2, 2, 5, 4, 2, 3, 4, 3, 4, 3, 2, 4, 2, 3, 2, 3, 4, 4, 5, 3, 4],
     "Perception of Improvement": [3, 3, 2, 2, 3, 2, 3, 1, 2, 2, 3, 3, 2, 1, 1, 3, 3, 3, 1, 3, 3, 3, 3, 1, 3, 2, 3, 2, 1, 3, 2, 3, 3, 3, 2, 2, 3, 2, 3, 2]
 }
 
-# Criar DataFrame
+
 df = pd.DataFrame(data)
 
-# Agrupar dados para calcular a média dos benefícios relatados por frequência de uso
 grouped_data = df.groupby('Frequency of use')['Perception of Improvement'].mean().reset_index()
 
-# Verificar a inversão dos dados (se necessário, inverta aqui)
 grouped_data['Perception of Improvement'] = grouped_data['Perception of Improvement'].values[::-1]
 
-# Imprimir os dados agrupados para verificação
 print(grouped_data)
 
-# Definir os rótulos dos ticks do eixo x
-# tick_labels = ['Always', 'Frequently', 'Occasionally', 'Rarely', 'Never']
-tick_labels = ['Never', 'Rarely', 'Occasionally', 'Frequently', 'Always']
+tick_labels = ['Rarely', 'Occasionally', 'Frequently', 'Always']
 # Gráfico de Barras: Frequency of Use and Perception of Improvement
 plt.figure(figsize=(10, 6))
 plt.bar(grouped_data['Frequency of use'], grouped_data['Perception of Improvement'], color='skyblue', edgecolor='black')
